c276be402bed2b8454b0f49dde4c08eec0baa150
TheSK653 <sohamkundu653@gmail.com>
2025-11-27 11:56:30 +0530

    update

---
+++ Bit_masking/LeftShift.java
@@ -0,0 +1,12 @@
+public class LeftShift {
+    public static void main(String[] args) {
+        int n=13;
+        System.out.println(n<<1);
+        int n1=-13;
+        System.out.println(n1<<1);
+        // byte b=-128;
+        // b = (byte) (b<<1);
+        n = Integer.MIN_VALUE<<1;
+        System.out.println(n);
+    }
+}
---
+++ Bit_masking/RightShift.java
@@ -0,0 +1,12 @@
+public class RightShift {
+    public static void main(String[] args) {
+        int n = 13;
+        System.out.println(n >> 1);
+        int n1 = -13;
+        System.out.println(n1 >> 1);
+        int a = -22;
+        System.out.println(a >> 2);
+        int b = -1;
+        System.out.println(b >> 2);
+    }
+}
